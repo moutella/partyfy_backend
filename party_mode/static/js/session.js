091aea32326992511911fe3bd6d4ed521af5ad9a
Moutella <joaogabrielmsc@gmail.com>
2020-01-26 16:35:53 -0300

    basic song search working

---
+++ party_mode/static/js/session.js
@@ -0,0 +1,79 @@
+jQuery(document).ready(function () {
+    var tracks = []
+
+    function create_searchs() {
+        console.log(jQuery("#song_name").val());
+        jQuery.ajax({
+            url: 'https://api.spotify.com/v1/search?type=track&q=' + jQuery("#song_name").val(),
+            headers: {
+                'Authorization': 'Bearer ' + data.token
+            },
+            success: function (response) {
+                tracks = []
+                for (var track in response.tracks.items) {
+                    var track_data = {
+                        label: response.tracks.items[track].name,
+                        value: response.tracks.items[track].uri,
+                        cover: response.tracks.items[track].album.images[0].url,
+                        album: response.tracks.items[track].album.name,
+                        artist: response.tracks.items[track].album.artists[0].name
+                    }
+                    tracks.push(track_data);
+                }
+            },
+            complete: function () {
+                updateAutoComplete();
+                jQuery("#song_name").autocomplete("search", jQuery("#song_name").val());
+            }
+        });
+    }
+
+    function addTextAreaCallback(textArea, callback, delay) {
+        var timer = null;
+        textArea.onkeypress = function () {
+            if (timer) {
+                window.clearTimeout(timer);
+            }
+            timer = window.setTimeout(function () {
+                timer = null;
+                callback();
+            }, delay);
+        };
+        textArea = null;
+    }
+
+    addTextAreaCallback(document.getElementById("song_name"), create_searchs, 350);
+
+    function updateAutoComplete() {
+        jQuery("#song_name").autocomplete({
+                minLength: 0,
+                source: tracks,
+                focus: function (event, ui) {
+                    jQuery("#song_name").val(ui.item.label);
+                    return false;
+                },
+                select: function (event, ui) {
+                    jQuery("#song_name").val(ui.item.label);
+                    jQuery("#request_song_uri").val(ui.item.value);
+                    jQuery("#request_song_form").submit();
+                }
+            })
+            .autocomplete("instance")._renderItem = function (ul, item) {
+                console.log(item);
+                return $("<li>")
+                    .append("<div class='row'>" +
+                        "<div class='col-2'><img style='width: 100%'src=" +
+                        item.cover +
+                        "></img></div>" +
+                        item.label +
+                        "<br>" +
+                        item.artist + "</div>"
+                    )
+                    .appendTo(ul);
+            };
+        jQuery.ui.autocomplete.prototype._resizeMenu = function () {
+            var ul = this.menu.element;
+            ul.outerWidth(this.element.outerWidth());
+        }
+    }
+});
--- party_mode/urls.py
+++ party_mode/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from .views import Login, LoginReturnPage, SessionView, MusicRequestView, Home
+from .views import (
+    Login,
+    LoginReturnPage,
+    SessionView,
+    MusicRequestView,
+    Home,
+    SongSearch
+)
 urlpatterns = [
     path('login/', Login.as_view(), name="login"),
     path('spotify_return/', LoginReturnPage.as_view(), name="login_return"),
     path('session/<str:session_id>/', SessionView.as_view(), name="session"),
     path('request/', MusicRequestView.as_view(), name="music_request"),
-    path('', Home.as_view(), name="home")
+    path('', Home.as_view(), name="home"),
+    path('search/', SongSearch.as_view(), name="search")
 ]
